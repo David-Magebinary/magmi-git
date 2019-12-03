62e7b371be535d6b55e5e4ea2d3622a8274c156a
Siyu Qian <david@magebinary.com>
2019-12-03 15:56:13 +1300

    SP-2214 Notify Playtech when the import job started

--- engine/plugins/base/general/emailreport/emailreport.php
+++ engine/plugins/base/general/emailreport/emailreport.php
@@ -2,22 +2,65 @@
 
 class EmailReportPlugin extends Magmi_GeneralImportPlugin
 {
+    /**
+     * @var string
+     */
+    const HTML_NEW_LINE = '<br />';
+
     protected $_attach;
+
+    /**
+     * @var array
+     */
+    protected $_params;
 
     public function initialize($params)
     {
         $this->_attach = array();
+        $this->_params = $params;
     }
 
     public function getPluginInfo()
     {
-        return array("name"=>"Import Report Mail Notifier","author"=>"Dweeves","version"=>"1.0.0",
-            "url"=>$this->pluginDocUrl("Import_report_mail_notifier"));
+        return array(
+            "name"      => "Import Report Mail Notifier",
+            "author"    => "David Qian",
+            "version"   => "1.1.0",
+            "url"       =>  $this->pluginDocUrl("Import_report_mail_notifier")
+        );
+    }
+
+    public function beforeImport()
+    {
+        $engine = $this->_callers[0];
+        $content = '';
+        $datasource = $engine->getPluginInstanceByClassName('datasources', 'Magmi_CSVDataSource');
+        if ($datasource != null) {
+            $csvfile = $datasource->getParam('CSV:filename');
+
+            $content .= '<html><body>';
+            $content .= sprintf('<b>Import Mode:</b> %s', $this->getMode()) . self::HTML_NEW_LINE;
+            $content .= sprintf('<b>Profile:</b> %s', $this->_params['profile']) . self::HTML_NEW_LINE;
+            $content .= '<b>Messages:</b>' . self::HTML_NEW_LINE;
+            $content .= sprintf('The import job for file %s is going to start.', $csvfile) . self::HTML_NEW_LINE;
+            $message .= '</body></html>';
+
+            $this->addAttachment($csvfile);
+        }
+
+        $response = $this->send_email($this->getParam("EMAILREP:to"), $this->getParam("EMAILREP:from"), $this->getParam("EMAILREP:from_alias", ""), "BinaryConnect before import notice", $content, $this->getAttachment());
+
+        // clean up the attachement array before sending out the next email
+        $this->_attach = [];
+
+        if (!$response) {
+            $this->log("Cannot send email", "error");
+        }
     }
 
     public function send_email($to, $from, $from_name, $subject, $message, $attachments = false)
     {
-        $headers = "From: " . $from_name . "<" . $from . ">\n";
+        $headers = 'From: ' . $from_name . "<" . $from . ">\n";
         $headers .= "Reply-To: " . $from_name . "<" . $from . ">\n";
         $headers .= "Return-Path: " . $from_name . "<" . $from . ">\n";
         $headers .= "Message-ID: <" . time() . "-" . $from . ">\n";
@@ -35,42 +78,42 @@ class EmailReportPlugin extends Magmi_GeneralImportPlugin
         $email_txt .= $msg_txt;
         $email_message = $email_txt;
         $email_message .= "This is a multi-part message in MIME format.\n\n" . "--{$mime_boundary}\n" .
-             "Content-Type:text/html; charset=\"iso-8859-1\"\n" . "Content-Transfer-Encoding: 7bit\n\n" . $email_txt .
-             "\n\n";
+        "Content-Type:text/html; charset=\"iso-8859-1\"\n" . "Content-Transfer-Encoding: 7bit\n\n" . $email_txt .
+        "\n\n";
 
         $attachments = $this->_attach;
         if ($attachments !== false) {
 
-            //Should we zip them?
+        //Should we zip them?
             $zip = $this->getParam("EMAILREP:attachcsv",false);
-	    $this->log("Zip: $zip", "info");	
-	    if ($zip){
+            $this->log("Zip: $zip", "info");
+            if ($zip){
                 $archive = new ZipArchive();
                 $fname = sys_get_temp_dir() . '/report.zip';
-		if ($archive->open($fname,ZipArchive::OVERWRITE) === true){
-		    for ($i = 0; $i < count($attachments); $i++){
-			if (!is_file($attachments[$i])) continue;
-			$fileatt_name = explode(DIRECTORY_SEPARATOR,$attachments[$i]);
-			$fileatt_name = array_pop($fileatt_name);
-			$archive->addFile($attachments[$i],$fileatt_name);
-                    }
-                    $archive->close();
+                if ($archive->open($fname,ZipArchive::OVERWRITE) === true){
+                  for ($i = 0; $i < count($attachments); $i++){
+                     if (!is_file($attachments[$i])) continue;
+                     $fileatt_name = explode(DIRECTORY_SEPARATOR,$attachments[$i]);
+                     $fileatt_name = array_pop($fileatt_name);
+                     $archive->addFile($attachments[$i],$fileatt_name);
+                 }
+                 $archive->close();
 
-                    $fileatt = $fname;
-                    $fileatt_type = "application/octet-stream";
-                    $fileatt_name = "report.zip";
-                    $file = fopen($fileatt,'rb');
-                    $data = fread($file,filesize($fileatt));
-		    fclose($file);
-                    $data = chunk_split(base64_encode($data));
+                 $fileatt = $fname;
+                 $fileatt_type = "application/octet-stream";
+                 $fileatt_name = "report.zip";
+                 $file = fopen($fileatt,'rb');
+                 $data = fread($file,filesize($fileatt));
+                 fclose($file);
+                 $data = chunk_split(base64_encode($data));
 
-                    $email_message .= "--{$mime_boundary}\n" . "Content-Type: {$fileatt_type};\n" .
-                         " name=\"{$fileatt_name}\"\n" . "Content-Transfer-Encoding: base64\n\n" . $data . "\n\n";
-		}else{
-                    $email_message .= "\n\nThere was a problem compressing your report\n\n";
-                }
+                 $email_message .= "--{$mime_boundary}\n" . "Content-Type: {$fileatt_type};\n" .
+                 " name=\"{$fileatt_name}\"\n" . "Content-Transfer-Encoding: base64\n\n" . $data . "\n\n";
+             } else {
+                $email_message .= "\n\nThere was a problem compressing your report\n\n";
+            }
 
-            }else{
+            } else {
 
                 for ($i = 0; $i < count($attachments); $i++) {
                     if (is_file($attachments[$i])) {
@@ -87,7 +130,7 @@ class EmailReportPlugin extends Magmi_GeneralImportPlugin
                         $data = chunk_split(base64_encode($data));
 
                         $email_message .= "--{$mime_boundary}\n" . "Content-Type: {$fileatt_type};\n" .
-                             " name=\"{$fileatt_name}\"\n" . "Content-Transfer-Encoding: base64\n\n" . $data . "\n\n";
+                        " name=\"{$fileatt_name}\"\n" . "Content-Transfer-Encoding: base64\n\n" . $data . "\n\n";
                     }
                 }
             }
@@ -103,6 +146,11 @@ class EmailReportPlugin extends Magmi_GeneralImportPlugin
     {
         $this->_attach[] = $fname;
         $this->_attach = array_unique($this->_attach);
+    }
+
+    public function getAttachment()
+    {
+        return $this->_attach;
     }
 
     public function getPluginParams($params)
